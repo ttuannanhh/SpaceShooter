f4e303091f5d99ae2663b9458c3bdd7842ddb159
ttuannanhh <113655800+ttuannanhh@users.noreply.github.com>
2024-05-25 11:50:29 +0700

    Add files via upload

---
+++ SpaceShooter/desktop/src/ca/grasley/spaceshooter/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package ca.grasley.spaceshooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.grasley.spaceshooter.SpaceShooterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 640;
+		config.width = 360;
+		new LwjglApplication(new SpaceShooterGame(), config);
+	}
+}
